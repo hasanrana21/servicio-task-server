eae3deb9a889a039a36d6c62b0be7e8d674c5357
hasanrana21 <hasanrana178821@gmail.com>
2021-11-25 21:46:27 +0600

    delete copleted and updating inprogress

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
+const ObjectID = require('mongodb').ObjectID;
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -22,20 +23,26 @@ client.connect((err) => {
     console.log(req.body);
     userCollection.insertOne(req.body).then((data) => {
       res.send(data.insertedCount > 0);
-      console.log(data);
+    //   console.log(data);
     });
   });
 
   app.get("/getUsers", (req, res) => {
     userCollection.find().toArray((err, items) => {
       res.send(items);
-      console.log(items);
+    //   console.log(items);
     });
   });
 
   app.delete('/delete/:id', (req, res) => {
       userCollection.deleteOne({_id: ObjectID(req.params.id)})
   })
+
+  app.patch('/updateUser/:id', (req, res) => {
+    var updateObject = req.body; // {last_name : "smith", age: 44}
+    var id = req.params.id;
+    db.users.update({_id  : ObjectId(id)}, {$set: updateObject});
+});
 
   app.get("/", (req, res) => {
     res.send("Mongodb Connencted");
