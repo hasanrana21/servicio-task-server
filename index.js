183dea54f078939e6136adfd383b93756a703a5c
hasanrana21 <hasanrana178821@gmail.com>
2021-11-25 19:51:51 +0600

    user data show on ui completed

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
-const express = require('express')
-const { MongoClient } = require('mongodb');
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h7tlf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const userCollection = client.db(`${process.env.DB_NAME}`).collection("allUsers");
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const userCollection = client
+    .db(`${process.env.DB_NAME}`)
+    .collection("allUsers");
 
-  app.post('/newUser', (req, res) => {
-    console.log(req.body)
-      userCollection.insertOne(req.body)
-      .then(data => {
-          res.send(data.insertedCount > 0);
-          console.log(data)
-      })
+  app.post("/newUser", (req, res) => {
+    console.log(req.body);
+    userCollection.insertOne(req.body).then((data) => {
+      res.send(data.insertedCount > 0);
+      console.log(data);
+    });
+  });
+
+  app.get("/getUsers", (req, res) => {
+    userCollection.find().toArray((err, items) => {
+      res.send(items);
+      console.log(items);
+    });
+  });
+
+  app.delete('/delete/:id', (req, res) => {
+      userCollection.deleteOne({_id: ObjectID(req.params.id)})
   })
 
-
-
-  app.get('/', (req, res) => {
-      res.send('Mongodb Connencted')
-  })
+  app.get("/", (req, res) => {
+    res.send("Mongodb Connencted");
+  });
 });
 
-app.listen(5050)
+app.listen(5050);
